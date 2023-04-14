7801b0c920490258a8d3d20ab74ca40145902159
kongtaesic <xotlr333@naver.com>
2023-04-14 22:27:09 +0900

    수 정렬하기 3

---
+++ Solution17.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * 백준
+ * 수 정렬하기 3
+ * https://www.acmicpc.net/problem/10989
+ */
+
+public class Solution17 {
+
+    // 계수 정렬(Counting Sort) 알고리즘
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(input.readLine());
+        int[] arr = new int[10000];
+
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(input.readLine());
+            arr[num] += 1;
+        }
+
+        for(int i = 0; i < 10000; i++) {
+            if(arr[i] > 0) {
+                for(int j = 0; j < arr[i]; j++ ){
+                    System.out.println(i);
+                }
+            }
+        }
+
+    }
+
+}
+
+
